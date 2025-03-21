490a919f4916a2d8e466fed1493b640687b4cef3
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2025-03-21 14:49:51 +0100

    fixed logs & cleanup functions

--- PlantCare.ino
+++ PlantCare.ino
@@ -8,12 +8,18 @@
 
 int waterPump = A4;
 int moiSensorPin = A5;
-int moiPowerPin = 13; 
-
+int powerPin = 2;
 int waterPumpCount = 0;
 int moiSensorValue = 0; 
 int temperature = 0;
 int humidity = 0;
+int lightSensorPin = A3;
+int lightSensorValue = 0;
+int waterLevel = 0;
+int waterVolume = 0;
+int distance = 0;
+int litersPerCm = 1.09;
+int button = 12;
 
 DHT11 dht11(8);
 WiFiUDP ntpUDP;
@@ -26,16 +32,14 @@ char password[] = SECRET_PASSWORD;
 
 WiFiServer server(443); 
 
-unsigned long previousMillis = 0;
-const long interval = 3600000;
-
 void setup() {
   Serial.begin(9600);
   wifiSetup();
   server.begin();
   pinMode(waterPump, OUTPUT);
-  pinMode(moiPowerPin, OUTPUT); 
-  digitalWrite(moiPowerPin, LOW);
+  digitalWrite(waterPump, LOW);
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, LOW);
   timeClient.begin();
 }
 
@@ -59,27 +63,50 @@ void wifiSetup() {
   }
 }
 
-void moistureSensor() {
- digitalWrite(moiPowerPin, HIGH); 
- delay(100); 
- moiSensorValue = analogRead(moiSensorPin); 
- digitalWrite(moiPowerPin, LOW); 
- moiSensorValue = map(moiSensorValue, 0, 1023, 100, 0); 
+void fetchData () {
+  distance = ultrasonic.read();
+  waterLevel = max(44 - distance, 0); 
+  waterVolume = (waterLevel * litersPerCm);
+  dht11.readTemperatureHumidity(temperature, humidity);
+  moiSensorValue = map(analogRead(moiSensorPin), 0, 1023, 100, 0);
+  lightSensorValue = map(analogRead(lightSensorPin), 0, 1023, 1000, 0);
+}
+
+void checkWaterLevelAndPump(int duration) {
+  distance = ultrasonic.read();
+  delay(500);
+  waterLevel = max(44 - distance, 0);
+
+  if (waterLevel > 2) {
+    digitalWrite(waterPump, HIGH);
+    delay(duration);
+    digitalWrite(waterPump, LOW);
+    waterPumpCount++;
+  }
 }
 
 void handleClient(WiFiClient client) {
+  digitalWrite(powerPin, HIGH);
+  delay(100);
+  fetchData();
+  delay(100);
   String currentLine = ""; 
-  int distance = ultrasonic.read();
-  float litersPerCm = 1.09;
-  float waterLevel = (44 - distance); 
+  timeClient.update(); 
+  int currentDay = timeClient.getDay();
+  int currentHours = timeClient.getHours();
+  int currentMinutes = timeClient.getMinutes();
+  int currentSeconds = timeClient.getSeconds();
 
-  if(waterLevel < 0) {
-    waterLevel = 0;
+  String weather = "";
+  if (lightSensorValue < 200) {
+    weather = "M&ouml;rkt";
+  } else if (lightSensorValue >= 200 && lightSensorValue < 990) {
+    weather = "Skugga";
+  } else if (lightSensorValue >= 990 && lightSensorValue < 995) {
+    weather = "Soligt";
+  } else if (lightSensorValue >= 995) {
+    weather = "Mycket soligt";
   }
-  float waterVolume = (waterLevel * litersPerCm);
-
-  dht11.readTemperatureHumidity(temperature, humidity);
-  moistureSensor(); 
 
   while (client.connected()) {
     if (client.available()) {
@@ -99,54 +126,53 @@ void handleClient(WiFiClient client) {
           client.println("<head><title>Plant Care+</title>");
           client.println("<meta name='viewport' content='width=device-width, initial-scale=1.0'>"); 
           client.println("<style>");
-          client.println("  body { font-family: Arial, sans-serif; background-color: #f2f2f2; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; flex-direction: column; min-height: 100vh; }");
+          client.println("  body { font-family: Arial, sans-serif; background-color: #cef8ff; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; flex-direction: column; min-height: 100vh; }");
           client.println("  h1 { color: #3e8e41; font-size: 28px; margin-bottom: 20px; text-align: center; }");
           client.println("  .container { width: 90%; max-width: 600px; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); box-sizing: border-box; }");
           client.println("  p { font-size: 16px; margin-bottom: 10px; color: #333; text-align: center; }");
           client.println("  .stat { font-weight: bold; }");
           client.println("  .value { color: #4CAF50; font-weight: normal; }");
-          client.println("  .card { background-color: #f9f9f9; padding: 15px; margin-bottom: 20px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); }");
+          client.println("  .card { background-color: #f9f9f94c; padding: 15px; margin-bottom: 20px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3); }");
           client.println("  button { background-color: #4CAF50; color: white; padding: 12px 20px; border: none; border-radius: 5px; cursor: pointer; font-size: 18px; display: block; width: 100%; margin: 10px 0; }");
           client.println("  button:hover { background-color: #45a049; }");
           client.println("  footer { margin-top: 30px; text-align: center; color: #777; font-size: 14px; }");
-          client.println("  @media (max-width: 768px) {");
-          client.println("    h1 { font-size: 24px; }");
-          client.println("    .container { width: 95%; }");
-          client.println("    p { font-size: 14px; }");
-          client.println("    button { font-size: 16px; }");
-          client.println("  }");
-          client.println("  @media (max-width: 480px) {");
-          client.println("    h1 { font-size: 22px; }");
-          client.println("    p { font-size: 12px; }");
-          client.println("  }");
           client.println("</style>");
           client.println("</head>");
           client.println("<body>");
           client.println("<div class='container'>");
 
           client.println("<div class='card'>");
+          client.println("<p><span class='stat'>V&auml;derlek:</span> <span class='value'>" + weather + " </span></p>");
           client.println("<p><span class='stat'>Temperatur:</span> <span class='value'>" + String(temperature) + " &deg;C</span></p>");
           client.println("<p><span class='stat'>Luftfuktighet:</span> <span class='value'>" + String(humidity) + " %</span></p>");
           client.println("<p><span class='stat'>Jordfuktighet:</span> <span class='value'>" + String(moiSensorValue) + " %</span></p>");
-          client.println("<p><span class='stat'>Antal vattningar idag:</span> <span class='value'>" + String(waterPumpCount) + " st</span></p>");
           client.println("<p><span class='stat'>Vattenvolym:</span> <span class='value'>" + String(waterVolume) + " L</span></p>");
+          client.println("<p><span class='stat'>Antal vattningar denna vecka:</span> <span class='value'>" + String(waterPumpCount) + " st</span></p>");
+          
           if (waterVolume < 2) {
             client.println("<p style='color: red; font-weight: bold;'>Varning: Fyll p&aring; vattentank!</p>");
           }
+          client.println("<button onclick='refresh()'>Uppdatera</button>");
           client.println("</div>");
-
-          client.println("<button onclick='refreshData()'>Uppdatera</button>");
-          client.println("</div>");
-
+          client.println("<script>");
+          client.println("function refresh() {");
+          client.println("  location.reload();"); 
+          client.println("}");
+          client.println("window.onload = function() {");
+          client.println("  setTimeout(function() {");
+          client.println("    console.log('Dag: ' + " + String(currentDay) + " + ' Tid: ' + " + String(currentHours) + " + ' Solstyrka: ' + " + String(lightSensorValue) + ");");
+          client.println("    console.log('Dag: ' + " + String(currentDay) + " + ' Tid: ' + " + String(currentHours) + " + ' Temperatur: ' + " + String(temperature) + ");");
+          client.println("    console.log('Dag: ' + " + String(currentDay) + " + ' Tid: ' + " + String(currentHours) + " + ' Luftfuktighet: ' + " + String(humidity) + ");");
+          client.println("    console.log('Dag: ' + " + String(currentDay) + " + ' Tid: ' + " + String(currentHours) + " + ' Jordfuktighet: ' + " + String(moiSensorValue) + ");");
+          client.println("  }, 3000);"); 
+          client.println("};");
+          client.println("setTimeout(function() {");
+          client.println(" location.reload();"); 
+          client.println("}, 3600000);"); 
+          client.println("</script>");
           client.println("<footer>");
           client.println("</footer>");
-
-          client.println("<script>");
-            client.println("function refreshData() {");
-             client.println("location.reload();");
-            client.println("}");
-          client.println("</script>");
-
+        
           client.println("</body>");
           client.println("</html>");
         } else {
@@ -158,11 +184,11 @@ void handleClient(WiFiClient client) {
       }
     }
   }
+  digitalWrite(powerPin, LOW);
   client.stop(); 
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
   WiFiClient client = server.available();
 
   if (client) {
@@ -170,42 +196,36 @@ void loop() {
   }
 
   timeClient.update(); 
-  int currentHour = timeClient.getHours();
-  int currentMinutes = timeClient.getMinutes();
-  int distance = ultrasonic.read();
-  float litersPerCm = 1.09;
-  float waterLevel = 44 - distance; 
+  int currentDay = timeClient.getDay();
+  int currentHours = timeClient.getHours();
+  int currentMinutes = timeClient.getMinutes(); 
+  int currentSeconds = timeClient.getSeconds();
 
-  if(waterLevel < 0) {
-    waterLevel = 0;
-  }
-
-  float waterVolume = waterLevel * litersPerCm;
-
-  if(currentHour == 0 && currentMinutes == 0) {
+  if(currentDay == 1 && currentHours == 0 && currentMinutes == 0) {
     waterPumpCount = 0;
   }
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    dht11.readTemperatureHumidity(temperature, humidity);
-    Serial.println("Temperature: " + String(temperature)+" °C");
-    Serial.println("Air-moisture: " + String(humidity) + " %"); 
-    moistureSensor(); 
-    Serial.println("Soil-moisture: " + String(moiSensorValue) + " %"); 
-    Serial.println("Waterpump count: " + String(waterPumpCount) + " st"); 
-    Serial.println("Water level: " + String(waterLevel) + " cm");
-    Serial.println("Water volume: " + String(waterVolume) + " L");
-    Serial.println("Distance: " + String(distance) + " cm");
-
-    if (waterLevel < 2) {
-        digitalWrite(waterPump, LOW);
-    } else if (moiSensorValue < 40) {
-      delay(5000);
-        digitalWrite(waterPump, HIGH);
-        delay(10000);
-        digitalWrite(waterPump, LOW);
-        waterPumpCount++;
-    }
+  if(digitalRead(button) == HIGH) {
+    digitalWrite(powerPin, HIGH);
+    delay(100);
+    checkWaterLevelAndPump(10000);
+    digitalWrite(powerPin, LOW);
   }
+  
+  if ((currentHours == 7 || currentHours == 13 || currentHours == 18) && 
+      currentMinutes == 0 && currentSeconds < 10) {
+      digitalWrite(powerPin, HIGH);
+      delay(100);
+
+      if (currentHours == 13) {
+        dht11.readTemperatureHumidity(temperature, humidity);
+        delay(500);
+        if (temperature >= 35) {
+          checkWaterLevelAndPump(60000);
+        }  
+      } else {
+        checkWaterLevelAndPump(120000);
+      }
+    digitalWrite(powerPin, LOW);
+   }
 }
